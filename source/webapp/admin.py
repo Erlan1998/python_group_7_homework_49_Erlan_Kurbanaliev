2c66d3ae7f92c1f30c3e2fabeb68b0c8e8edaba1
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-09 17:32:45 +0600

    создал модели и админку!

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from webapp.models import List, Status, Type
 # Register your models here.
+
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'tip', 'updated_at']
+    list_filter = ['status', 'tip']
+    search_fields = ['summary']
+    fields = ['id', 'summary', 'description', 'status', 'tip', 'updated_at']
+    readonly_fields = ['updated_at', 'id']
+
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    status = models.CharField(max_length= 50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.status}'
+
+class Type(models.Model):
+    tip = models.CharField(max_length= 50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.tip}'
+
+
+class List(models.Model):
+    summary = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=3000, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.ForeignKey('webapp.Status', related_name='Lists', on_delete=models.PROTECT)
+    tip = models.ForeignKey('webapp.Type', related_name='List', on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'Lists'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.status}: {self.description}'
