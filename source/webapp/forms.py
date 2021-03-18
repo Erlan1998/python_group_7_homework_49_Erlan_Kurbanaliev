7ff296d516f6fae9b47ff205b865bd2688494cd7
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-18 16:41:37 +0600

    Написал форму для модели!

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.forms import widgets
-from webapp.models import Status, Type
+from webapp.models import Status, Type, List
 
 
-class ListForm(forms.Form):
-    summary = forms.CharField(max_length=100, label='Заголовок')
-    description = forms.CharField(max_length=3000, widget=widgets.Textarea(), required=False, label='Описание')
-    status = forms.ModelChoiceField(queryset=Status.objects.all())
-    tip = forms.ModelMultipleChoiceField(required=False, label='Тип', queryset=Type.objects.all())
+class ListForm(forms.ModelForm):
+
+    class Meta:
+        model = List
+        fields = ['summary', 'description', 'status', 'tip']
+
