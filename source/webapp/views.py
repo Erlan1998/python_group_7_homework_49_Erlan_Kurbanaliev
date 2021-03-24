836507c3ac3b4c7caeed764e298a3bd3c8f1a78c
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-24 16:34:26 +0600

    переписал IndexView. теперь он наследуется не от TemplateView а от ListView! добавил 3 новых метода!

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,16 +1,48 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View, TemplateView
+from django.views.generic import TemplateView, FormView, ListView
 from webapp.models import List
-from webapp.forms import ListForm
+from webapp.forms import ListForm, SearchForm
+from django.urls import reverse
+from django.db.models import Q
+from django.utils.http import urlencode
 # Create your views here.
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
+    model = List
+    context_object_name = 'lists'
+    ordering = ('-created_at', 'summary')
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(IndexView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(summary__icontains=self.search_data) |
+                Q(status__status__icontains=self.search_data) |
+                Q(tip__tip__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
 
     def get_context_data(self, **kwargs):
-        kwargs['lists'] = List.objects.all()
-        return super().get_context_data(**kwargs)
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['search_form'] = self.form
+
+        if self.search_data:
+            kwargs['query'] = urlencode({'search_value': self.search_data})
+
+        return kwargs
 
 
 class TaskView(TemplateView):
