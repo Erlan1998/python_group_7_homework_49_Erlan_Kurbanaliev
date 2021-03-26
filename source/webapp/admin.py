0221bd183910e9f1e3ac387967680e47a0b913a9
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-03-26 17:49:55 +0600

    сделал Админку!

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
-from webapp.models import List, Status, Type
+from webapp.models import List, Status, Type, Porjects
 # Register your models here.
 
 
 class ListAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'status', 'updated_at']
+    list_display = ['id', 'summary', 'status', 'updated_at', 'project']
     list_filter = ['status', 'tip']
     search_fields = ['summary']
-    fields = ['id', 'summary', 'description', 'status', 'tip', 'updated_at']
+    fields = ['id', 'summary', 'description', 'status', 'tip', 'updated_at', 'project']
     readonly_fields = ['updated_at', 'id']
 
 
 admin.site.register(List, ListAdmin)
+admin.site.register(Porjects)
 admin.site.register(Status)
 admin.site.register(Type)
