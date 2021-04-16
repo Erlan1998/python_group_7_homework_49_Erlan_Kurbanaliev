71ae9a7f48e4746d182f40b6b7f1f16e065f1f92
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-17 00:47:45 +0600

    сделал верстку на детальный просмотр пользователя и его проектов!

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from accounts.views import register_view
+from accounts.views import register_view, UserDetailView, UserView
 from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('registration/', register_view, name='register')
+    path('registration/', register_view, name='register'),
+    path('accounts/index/', UserView.as_view(), name='index_accounts'),
+    path('accounts/<int:id>/', UserDetailView.as_view(), name='all_accounts'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from accounts.forms import UserRegisterForm
 from django.shortcuts import render, redirect
 
@@ -13,3 +17,34 @@ def register_view(request, *args, **kwargs):
             return redirect('index_project')
     context['form'] = form
     return render(request, 'registration/registe.html', context=context)
+
+
+
+class UserView(ListView):
+    model = get_user_model()
+    template_name = 'index_users.html'
+    context_object_name = 'user_object'
+    pk_url_kwarg = 'id'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+
+class UserDetailView(DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    pk_url_kwarg = 'id'
+    paginate_related_by = 5
+    paginate_related_orphans = 1
+
+    def get_context_data(self, **kwargs):
+        projects = self.get_object().projects.all()
+        print(projects)
+        paginator = Paginator(projects, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        print(page_number)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
