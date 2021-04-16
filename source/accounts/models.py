1169a8bdda2dcc3163ce0491567a7c34989bbd6c
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-16 16:57:25 +0600

    написал модельку и применил миграции!

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    github = models.URLField(max_length=250, blank=True, null=True)
+    about = models.TextField(max_length=150, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
