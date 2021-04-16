bfb6a5257b1d5ef868d2386f401034d15890fe3d
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-17 03:24:12 +0600

    сделал ссылочки на переход по именам пользователей!

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.utils.http import urlencode
 from django.views.generic import DetailView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from accounts.forms import UserRegisterForm
 from django.shortcuts import render, redirect
 
+from webapp.forms import SearchForm
 
 
 def register_view(request, *args, **kwargs):
@@ -19,7 +21,6 @@ def register_view(request, *args, **kwargs):
     return render(request, 'registration/registe.html', context=context)
 
 
-
 class UserView(ListView):
     model = get_user_model()
     template_name = 'index_users.html'
@@ -27,6 +28,35 @@ class UserView(ListView):
     pk_url_kwarg = 'id'
     paginate_related_by = 5
     paginate_related_orphans = 0
+
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(UserView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(username__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['search_form'] = self.form
+
+        if self.search_data:
+            kwargs['query'] = urlencode({'search_value': self.search_data})
+
+        return kwargs
 
 
 class UserDetailView(DetailView):
@@ -39,10 +69,8 @@ class UserDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         projects = self.get_object().projects.all()
-        print(projects)
         paginator = Paginator(projects, self.paginate_related_by, orphans=self.paginate_related_orphans)
         page_number = self.request.GET.get('page', 1)
-        print(page_number)
         page = paginator.get_page(page_number)
         kwargs['page_obj'] = page
         kwargs['projects'] = page.object_list
