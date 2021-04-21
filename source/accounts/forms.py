7c11fd985184e35823c8fad04e1f4916135727b5
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-21 18:12:35 +0600

    написал форму для изменения данныхпользователя и для изменения пароля!

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from accounts.models import Profile
+from django.forms.widgets import PasswordInput
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.CharField(max_length=30, required=True)
@@ -14,3 +18,48 @@ class UserRegisterForm(UserCreationForm):
         super(UserRegisterForm, self).clean()
         if not self.cleaned_data.get("first_name") and not self.cleaned_data.get("last_name"):
             raise ValidationError('First name or Last name should be reqistered')
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'first_name', 'last_name')
+
+
+class UserChangePasswordForm(forms.ModelForm):
+    old_password = forms.CharField(required=True, label='Старый пароль', widget=PasswordInput)
+    new_password = forms.CharField(required=True, label='Новый пароль', widget=PasswordInput)
+    password_confirm = forms.CharField(required=True, label='Подтвердите пароль', widget=PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('old_password', 'new_password', 'password_confirm')
+
+    def clean_password_confirm(self):
+        new_password = self.cleaned_data.get('new_password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+
+        if new_password != password_confirm:
+            raise forms.ValidationError('Пароли не совподают!')
+        return new_password
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Пароль введен не верно!')
+
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data.get('new_password'))
+        if commit:
+            user.save()
+        return user
