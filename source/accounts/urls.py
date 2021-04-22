470536c5b9a482394f6d6031b9b82e530b6a5410
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-22 18:13:12 +0600

    Написал верстку для страниц изменения личных данных пользователя и его профиля. и так же написал верстку для изменения пароля.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', register_view, name='register'),
-    path('accounts/index/', UserView.as_view(), name='index_accounts'),
-    path('accounts/<int:id>/', UserDetailView.as_view(), name='all_accounts'),
+    path('index/', UserView.as_view(), name='index_accounts'),
+    path('<int:id>/', UserDetailView.as_view(), name='all_accounts'),
     path('profile/', UserUpdateView.as_view(), name='update_user'),
     path('profile/change-password', UserChangePasswordView.as_view(), name='change_password'),
 ]
