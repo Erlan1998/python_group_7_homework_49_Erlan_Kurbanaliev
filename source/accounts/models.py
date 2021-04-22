93ba88f0d0317a3216d4c9fd18cbc0ab6c14af46
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-22 18:10:30 +0600

    добавил имена полям в модельке. и применил миграции

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь', null=True)
     avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
-    github = models.URLField(max_length=250, blank=True, null=True)
-    about = models.TextField(max_length=150, null=True, blank=True)
+    github = models.URLField(max_length=250, blank=True, null=True, verbose_name='Ссылка на Github:')
+    about = models.TextField(max_length=150, null=True, blank=True, verbose_name='О себе:')
 
     class Meta:
         verbose_name = 'Профиль'
